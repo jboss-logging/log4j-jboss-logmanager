daf7edd12a4c80b4af845a0b7993859de197a1d6
James Perkins <jperkins@redhat.com>
2013-04-23 10:22:22 -0700

    Fix ThrowableInformation serialization.

---
+++ src/test/java/org/apache/log4j/LoggingEventTest.java
@@ -0,0 +1,67 @@
+/*
+ * Modifications by Red Hat, Inc.
+ *
+ * This file incorporates work covered by the following notice(s):
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.log4j;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
+
+import junit.framework.Assert;
+import org.apache.log4j.spi.LoggingEvent;
+import org.jboss.logmanager.ExtLogRecord;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+public class LoggingEventTest {
+
+    @Test
+    public void testSerialization() throws Exception {
+        final Exception e = new Exception("testException");
+        final String category = "org.apache.log4j.test";
+        final Logger logger = Logger.getLogger(category);
+        final ExtLogRecord logRecord = new ExtLogRecord(org.jboss.logmanager.Level.INFO, "A test logging event", category);
+        logRecord.setThrown(e);
+        final LoggingEvent loggingEvent = new LoggingEvent(logRecord, logger);
+        // Serialize the logging event
+        final ByteArrayOutputStream out = new ByteArrayOutputStream();
+        final ObjectOutputStream objOut = new ObjectOutputStream(out);
+        try {
+            objOut.writeObject(loggingEvent);
+        } finally {
+            objOut.close();
+        }
+
+        final ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+        final ObjectInputStream objIn = new ObjectInputStream(in);
+        try {
+            final LoggingEvent deserializedEvent = (LoggingEvent) objIn.readObject();
+            final String[] array = deserializedEvent.getThrowableStrRep();
+            Assert.assertEquals(Arrays.toString(loggingEvent.getThrowableStrRep()), Arrays.toString(array));
+        } finally {
+            objIn.close();
+        }
+    }
+}
